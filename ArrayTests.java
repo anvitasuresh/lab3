637a5b2055224b8360daf5a637d0b940a10b48b1
anvitasuresh <ansuresh@ucsd.edu>
2023-04-19 17:46:57 -0700

    .

--- ArrayTests.java
+++ ArrayTests.java
@@ -9,10 +9,23 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2(){
+    int[] input2 = {5,6,7};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{7,6,5}, input2);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReversed1(){
+    int[] input2 = {1,2,3,4};
+    assertArrayEquals(new int[]{4,3,2,1}, ArrayExamples.reversed(input2));
+  }
+
 }
